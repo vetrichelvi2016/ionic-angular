6a7303f5025e13c49c8f2cf046fc6779b93ea6f9
vetrichelvi2016 <sribalaji1004@outlook.com>
2017-06-01 17:23:55 +0530

    updated

--- www/js/contactController.js
+++ www/js/contactController.js
@@ -1,4 +1,4 @@
-app.controller('contactController',function($scope,$stateParams){
+app.controller('contactController',function($scope,$stateParams,$http){
 
   // $scope.getlist = function(){
   $scope.contactList = [
@@ -9,5 +9,20 @@ app.controller('contactController',function($scope,$stateParams){
     { title: 'Rap', id: 5 },
     { title: 'Cowbell', id: 6 }
   ];
-// }
+ $scope.getCustomerList= function(){
+   var getdata = $http.get('https://devcmnh.azurewebsites.net/odata/')
+   .success(function(data,status,headers,config){
+     $scope.Details = data;
+   })
+   getdata.$promise.then(
+     function(data){
+       angular.forEach(data.value),function(index,item){
+         alert(index.data)
+        var cusValue = index.data;
+       }
+
+     }
+   )
+ }
+
 });
